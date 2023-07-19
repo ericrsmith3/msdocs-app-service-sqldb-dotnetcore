fb5ac44d8071ff216a0c15f0dac47055ae3d9442
ericrsmith3 <139991845+ericrsmith3@users.noreply.github.com>
2023-07-19 12:57:32 -0500

    test

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Data Source=st4-mwha-caringforiowa-server.database.windows.net,1433;Initial Catalog=st4-mwha-caringforiowa-database;User ID=st4-mwha-caringforiowa-server-admin;Password=4VJN5460U1DDDAIT$")));
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+    options.Configuration = builder.Configuration["st4-mwha-caringforiowa-cache.redis.cache.windows.net:6380,password=W7qpKWfweBfTcVcH0m9V4vYoen7OintvmAzCaM6QsOk=,ssl=True,defaultDatabase=0"];
     options.InstanceName = "SampleInstance";
 });
 
